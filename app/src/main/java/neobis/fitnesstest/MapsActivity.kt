0c8616e2c42ef3b57c0c9f4f6bba85529d9f257b
aisa.tashmatova <aisa.tashmatova@gmail.com>
2018-05-24 17:20:20 +0600

    first commit

---
+++ app/src/main/java/neobis/fitnesstest/MapsActivity.kt
@@ -0,0 +1,134 @@
+package neobis.fitnesstest
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+
+class MapsActivity : AppCompatActivity(),
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+
+    private  var mMap: GoogleMap? = null
+    private val LOCATION_PERMISSION = 100
+    private var mLocationRequest: LocationRequest? = null
+    private var builder: LatLngBounds.Builder? = null
+    private var mGoogleApiClient: GoogleApiClient? = null
+    private var longitude: Double? = null
+    private var latitude: Double? = null
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        init()
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+
+    }
+    private fun init(){
+        initMyLoc()
+    }
+    private fun initMyLoc() {
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build()
+        }
+        if (mGoogleApiClient != null) {
+            this.mGoogleApiClient!!.connect()
+        }
+    }
+
+
+    override fun onConnected(p0: Bundle?) {
+        checkLocationPermission()
+        val mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient)
+        if (mLastLocation != null) {
+            latitude = mLastLocation.latitude
+            longitude = mLastLocation.longitude
+            Log.d("mm>>><<<", latitude.toString() + " " + longitude)
+            moveCameraDirection()
+            Toast.makeText(this, "lat"+ latitude + "long"+longitude , Toast.LENGTH_LONG).show()
+
+
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        mLocationRequest = LocationRequest()
+        mLocationRequest?.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION)
+            return false
+        }
+        mMap?.isMyLocationEnabled = true
+        return true
+    }
+
+    private fun moveCameraDirection() {
+        if (mMap != null && builder != null) {
+            try {
+                val updatePosition = CameraUpdateFactory.newLatLngBounds(builder?.build(), 500, 500, 5)
+                mMap?.animateCamera(updatePosition)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        } else if (builder == null && mMap != null) {
+            mMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(42.88200, 74.58274), 10f))
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+
+        when (requestCode) {
+            LOCATION_PERMISSION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // The permission request was granted, we set up the marker
+                    init()
+                } else {
+                    // The permission request was denied, we make the user aware of why the location is not shown
+                    Toast.makeText(this, "Since the permission wasn't granted we can't show the location", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+        }
+    }
+}
