cc9ac84186605d4bcfbfae5e7ed3ee0261fbc056
aisa.tashmatova <aisa.tashmatova@gmail.com>
2018-05-25 16:03:30 +0600

    yes

--- app/src/main/java/neobis/fitnesstest/MapsActivity.kt
+++ app/src/main/java/neobis/fitnesstest/MapsActivity.kt
@@ -1,7 +1,9 @@
 package neobis.fitnesstest
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
@@ -10,7 +12,6 @@ import android.widget.Toast
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -28,59 +29,45 @@ class MapsActivity : AppCompatActivity(),
     private val LOCATION_PERMISSION = 100
     private var mLocationRequest: LocationRequest? = null
     private var builder: LatLngBounds.Builder? = null
-    private var mGoogleApiClient: GoogleApiClient? = null
-    private var longitude: Double? = null
-    private var latitude: Double? = null
-
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+        checkLocationPermission()
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        init()
 
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-
     }
-    private fun init(){
-        initMyLoc()
-    }
-    private fun initMyLoc() {
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build()
-        }
-        if (mGoogleApiClient != null) {
-            this.mGoogleApiClient!!.connect()
-        }
-    }
-
 
     override fun onConnected(p0: Bundle?) {
-        checkLocationPermission()
-        val mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient)
-        if (mLastLocation != null) {
-            latitude = mLastLocation.latitude
-            longitude = mLastLocation.longitude
-            Log.d("mm>>><<<", latitude.toString() + " " + longitude)
-            moveCameraDirection()
-            Toast.makeText(this, "lat"+ latitude + "long"+longitude , Toast.LENGTH_LONG).show()
+        redirect()
+    }
 
+    private fun redirect(){
+        val mLastLocation = onLocation()
+        Log.i("______________", "${mLastLocation?.latitude} ${mLastLocation?.longitude}")// LocationServices.FusedLocationApi.getLastLocation( mGoogleApiClient)
+        if (mLastLocation != null) {
+            moveCameraDirection()
+            Toast.makeText(this, "lat"+ mLastLocation.latitude + "long"+mLastLocation.longitude , Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun onLocation() : LatLng? {
+        if(checkLocationPermission()) {
+            // instantiate the location manager, note you will need to request permissions in your manifest
+            val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            // get the last know location from your location manager.
+            val location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+            // now get the lat/lon from the location and do something with it.
+            return LatLng(location.latitude, location.longitude)
 
         }
+        return null
     }
 
     override fun onConnectionSuspended(p0: Int) {
@@ -93,7 +80,8 @@ class MapsActivity : AppCompatActivity(),
         mLocationRequest = LocationRequest()
         mLocationRequest?.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
         if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(applicationContext,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION)
             return false
@@ -120,9 +108,9 @@ class MapsActivity : AppCompatActivity(),
         when (requestCode) {
             LOCATION_PERMISSION -> {
                 // If request is cancelled, the result arrays are empty.
-                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // The permission request was granted, we set up the marker
-                    init()
+                    redirect()
                 } else {
                     // The permission request was denied, we make the user aware of why the location is not shown
                     Toast.makeText(this, "Since the permission wasn't granted we can't show the location", Toast.LENGTH_LONG).show()
